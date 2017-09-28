086fe805a465f02bbc6d13ba9c39168ea28ba3e1
Loes Kenis <loes.kenis@immalle.eu>
2017-09-28 09:27:46 +0200

    Got it working!

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -4,9 +4,31 @@ namespace WorldDbQuerier
 {
     class Program
     {
+        static string version = "0.1";
+
+        static void Version()
+        {
+            Console.WriteLine("WorldDbQuerier {0}", version);
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "-v":
+                        Version();
+                        break;
+                    default:
+                        Console.WriteLine("Onbekend argument");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hello World");
+            }
         }
     }
 }
