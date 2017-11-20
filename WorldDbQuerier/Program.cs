d07f7706c4547ef8378bc8c0983df572b89d8dc2
Loes Kenis <loes.kenis@immalle.eu>
2017-11-20 13:58:34 +0100

    Version 0.4

--- WorldDbQuerier/Program.cs
+++ WorldDbQuerier/Program.cs
@@ -5,7 +5,7 @@ namespace WorldDbQuerier
 {
     class Program
     {
-        static string version = "0.3";
+        static string version = "0.4";
 
         static void Version()
         {
@@ -50,6 +50,25 @@ namespace WorldDbQuerier
             rdr.Dispose();
             conn.Close();
         }
+        public static void SearchCountry(string searchParameter)
+        {
+            MySqlConnection comm = new MySqlConnection();
+            comm.ConnectionString = "Server=192.168.56.101;Port=3306;Database=world;Uid=imma;Pwd=ImmaPwd;";
+            MySqlCommand cmd = new MySqlCommand();
+            cmd.Connection = comm;
+
+            comm.Open();
+
+            cmd.CommandText = "SELECT Code, Name, Continent, Region FROM Country WHERE Name LIKE @searchParameter";
+            cmd.Parameters.AddWithValue("@searchParameter", "%" + searchParameter + "%");
+            var reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(String.Format("{0}", reader[1]));
+
+            }
+        }
 
         static void Main(string[] args)
         {
@@ -69,6 +88,7 @@ namespace WorldDbQuerier
             Console.WriteLine("Make a choice");
             Console.WriteLine("1. Show amount of countries in database");
             Console.WriteLine("2. Show all countries in database");
+            Console.WriteLine("3. Search for a country");
             choice = Console.ReadLine();
 
             switch(choice)
@@ -79,6 +99,11 @@ namespace WorldDbQuerier
                 case "2":
                     ShowAllCountries();
                     break;
+                case "3":
+                    Console.WriteLine("What country are you looking for?");
+                    string response2 = Console.ReadLine();
+                    Console.WriteLine("This is not the country you were looking for ;)");
+                    SearchCountry(response2); break;
             }
         }
     }
